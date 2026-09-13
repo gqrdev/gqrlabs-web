c7a5abb1a8c7c8bdecbdb8e5641af61800cecd7b
gqrdev <gqrdev@gmail.com>
2026-09-13 20:36:05 +0000

    Fix Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import {defineConfig} from "astro/config";
+import clarityIntegration from "astro-microsoft-clarity-integration";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
@@ -9,6 +10,17 @@ export default defineConfig({
 			transformer: "lightningcss",
 		},
 	},
+
+	integrations: [
+		clarityIntegration({
+			projectId: "yd4hrp2pj4",
+			enabled: import.meta.env.PROD,
+			scriptStage: "head-inline",
+			debug: false,
+			async: true,
+			defer: false,
+		}),
+	],
 
 	output: "static",
 	adapter: cloudflare({
